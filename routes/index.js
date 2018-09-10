1fc30ce0a17b0f364d9e6060cae9ea3f432427a0
JinGangRed <k15950590137@126.com>
2018-09-10 21:24:37 +0800

    初始化项目

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const user_routers = require('./users');
+const createError = require('http-errors');
+const configs = require('../configs/app.settings');
+const routes = function (app) {
+    app.use('/user',user_routers);
+    app.use('*',function (req,res,next) {
+        next(createError(404));
+    });
+    if(configs.environment === "DEV"){
+      app.use(ClientErrorHandler);
+    }else {
+      app.use(LogErrorHandler);
+    }
+};
+
+/**
+ * 处理掉所有的错误
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ * @constructor
+ */
+function LogErrorHandler(err,req,res,next){
+  console.error(err);
+  res.status(200).send('发生错误');
+}
+
+/**
+ * 客户端处理
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ * @constructor
+ */
+function ClientErrorHandler(err,req,res,next) {
+    next(err);
+}
+
+
+
+module.exports = routes;
